852ec76544db8ff9c6c24f87daadd082d24f5dd8
Amit Prajapati <amitprj796@gmail.com>
2024-05-13 21:19:30 +0530

    bug fixed

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -67,8 +67,10 @@ const BannerHome = () => {
                                             <span>|</span>
                                             <p>View : { Number(data.popularity).toFixed(0) }</p>
                                         </div>
-                                        <Link to={"/"+data?.media_type+"/"+data.id} className='bg-white px-4 py-2 text-black font-bold rounded mt-4  hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>
-                                            Play Now
+                                        <Link to={"/"+data?.media_type+"/"+data.id}>
+                                            <button  className=' bg-white px-4 py-2 text-black font-bold rounded mt-4  hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>
+                                                Play Now
+                                            </button>
                                         </Link>
                                     </div>
                                 </div>
